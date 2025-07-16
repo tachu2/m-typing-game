9d29788af3410cbf47a30ade1cc5fdeedecb629a
tachu <roatyungtatyu@gmail.com>
2025-07-17 02:22:08 +0900

    add: input state

--- src/app/typing-game/page.tsx
+++ src/app/typing-game/page.tsx
@@ -1,13 +1,26 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 
 export default function TypingGame() {
+  const [input, setInput] = useState("");
+
   const inputRef = useRef<HTMLInputElement>(null);
   const problem = "test";
+
+  const handleOnChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    if (e.target.value === problem) {
+      console.log("Correct!");
+      if (inputRef.current) {
+        setInput("");
+      }
+    }
+  }, []);
 
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="w-full max-w-2xl bg-white rounded-lg shadow-md p-8 flex flex-col items-center">
@@ -15,8 +28,10 @@ export default function TypingGame() {
         <input
           ref={inputRef}
           type="text"
+          value={input}
           placeholder="type here..."
           className="w-full max-w-md text-center text-lg p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={(e) => handleOnChange(e)}
         />
       </div>
     </div>
